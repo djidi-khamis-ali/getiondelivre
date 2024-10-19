f20a68a498615ff5781ee2e8342f35904376491c
Ali Djidi <300140112@monboreal.ca>
2024-10-19 18:22:50 -0400

    ajout du classe mere livre

---
+++ AppConsoleLivres/Livre.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace AppConsoleLivres
+{
+    internal class Livre
+    {
+        public string Titre {  get; set; }
+        public string Auteur { get; set; }
+        public int NombrePage { get; set; }
+
+        public Livre(string titre, string auteur, int nombrePage)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            NombrePage = nombrePage;
+
+        }
+        public virtual void AfficheDetails()
+        { Console.WriteLine($"Le livre a pour auteur {Auteur} et son titre est {Titre},le nombre de page est de {NombrePage} pages "); }
+    }
+}
