64bd9aab377f9febf3b7bc820ed6e02c8409031e
Ali Djidi <300140112@monboreal.ca>
2024-10-19 20:52:30 -0400

    ajout des instances revue

--- AppConsoleLivres/Program.cs
+++ AppConsoleLivres/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using AppConsoleLivres;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       Revue revue1=new Revue ("king","Ali",412,30,1990);
+       Revue revue2 = new Revue("Arbre", "Brahim", 1012, 80, 1880);
+       Revue revue3 = new Revue("Foret", "Saleh", 412, 30, 2000);
+
+        List<Revue>Revues = new List<Revue> { revue1,revue2,revue3};
+        Console.WriteLine("Revues sont : ");
+        foreach (var Revue in Revues) 
+        {
+            Revue.AfficheDetails();
+        }
     }
 }
---
+++ AppConsoleLivres/Revue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Revue : Livre
+    {
+        public int  Numero {  get; set; }
+        public int Annee  { get; set; }
+
+        public Revue (string titre,string auteur , int nombrepages , int numero ,int annee):base (titre ,auteur,nombrepages)
+        {
+            Numero = numero;
+            Annee = annee;
+
+        }
+
+        public override void AfficheDetails()
+        {
+            Console.WriteLine($"L'auteur du livre est {Auteur}et  le titre {Titre} et nombre de page {NombrePage},le numero de revue est {Numero}et l annee {Annee}");
+        }
+    }
+}
